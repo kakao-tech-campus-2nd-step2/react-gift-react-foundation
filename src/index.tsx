bcb4a5fb1a5f0a9839150c8712fe7962ba2ac4d5
KimKyuHoi <k546kh@gmail.com>
2024-06-25 17:21:49 +0900

    Chore: EsLint 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 
-import App from '@/App';
-import reportWebVitals from '@/reportWebVitals';
+import App from '@/App.tsx';
+import reportWebVitals from '@/reportWebVitals.ts';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
