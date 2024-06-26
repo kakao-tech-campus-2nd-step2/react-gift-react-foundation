d556c48fef675b3c3e5b49e306cce2632ba8be61
ikuwlghks@naver.com <ikuwlghks@naver.com>
2024-06-26 16:03:37 +0900

    feat: reset CSS 적용을 위한 reset.ts 파일 생성
    - styles 폴더에 reset.ts 파일 생성
    - Reset CSS를 적용하여 기본 스타일 초기화

---
+++ my-app/src/styles/reset.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react'
+
+const reset = css`
+  /* Reset CSS 작성 */
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html,
+  body {
+    height: 100%;
+    font-family: Arial, sans-serif;
+  }
+`
+
+export default reset
