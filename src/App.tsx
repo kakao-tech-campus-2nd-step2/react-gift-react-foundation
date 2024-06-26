304c76d14cfd05b507166aa97c9da458063fbaf0
JaeAnHan <jaean1999@naver.com>
2024-06-26 20:02:07 +0900

    feat : add and apply emotion and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Global } from '@emotion/react';
+import { ResetCss } from '../public/styles/reset.js';
 
 function App() {
-    return (
-        <Fragment>
-            <h1>Hello</h1>
-        </Fragment>
-    );
+  return (
+    <React.StrictMode>
+      <Global styles={ResetCss} />
+      <h1>Hello</h1>
+    </React.StrictMode>
+  );
 }
 
 export default App;
