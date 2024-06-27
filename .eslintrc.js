b6ab2b0f6df701844c81e88d742a1c69dde46425
joshuadesu <meron_pan@kakao.com>
2024-06-27 14:19:03 +0900

    Fix: Add devDependcy에 설치된 storybook 사용 오류 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,21 @@
 module.exports = {
-  env : {
-    browser: true,
-    es6: true,
-    node: true,
+  "env" : {
+    "browser": true,
+    "es6": true,
+    "node": true,
   },
-  extends: ['airbnb', 'plugin:storybook/recommended'],
+  "extends": ['airbnb', 'plugin:storybook/recommended'],
+  "rules": {
+  "import/no-extraneous-dependencies": [
+      "error",
+      {
+        "devDependencies": [
+          "**/*.stories.*",
+          "**/.storybook/**/*.*"
+        ],
+        "peerDependencies": true
+      }
+    ]
+  }
 };
+
