46f89ddf71a8d8cc93c2395d40970d5f41403342
minji2219 <200146@jnu.ac.kr>
2024-06-27 17:29:15 +0900

    Feat: path 별칭 작동 확인을 위한 import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from '@components/Button';
 
 function App() {
-  return <div className="App">dsd React</div>;
+  return (
+    <div className="App">
+      <Button>버튼입니다.</Button>
+    </div>
+  );
 }
 
 export default App;
