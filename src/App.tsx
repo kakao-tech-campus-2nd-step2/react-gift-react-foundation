a6c2b686ae1b3c620b0981ed010a5f94f887af73
tteokbokki-master <0jin.git@gmail.com>
2024-06-27 10:15:58 +0900

    chore: Emotion 설치 및 reset css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import './App.css';
+import { Global } from '@emotion/react';
+import resetStyle from '@/styles/resetStyle';
 
 function App() {
-  return <div className="App">안녕</div>;
+  return (
+    <div className="App">
+      <Global styles={resetStyle} />
+      안녕
+    </div>
+  );
 }
 
 export default App;
