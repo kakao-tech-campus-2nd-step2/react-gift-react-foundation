3c1ad6dc31e352dbe6e23140cec0ae3d85f9c515
eunjin <eunjin0321@naver.com>
2024-06-27 14:24:41 +0900

    chore: eslintrc.json eslint.config.msj에 통합 후 삭제

--- react-gift/eslint.config.mjs
+++ react-gift/eslint.config.mjs
@@ -1,14 +1,50 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import prettier from "eslint-plugin-prettier";
+import airbnb from "eslint-config-airbnb";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      react: pluginReactConfig,
+      reactHooks: require("eslint-plugin-react-hooks"),
+      ts: tseslint,
+      jsxA11y: require("eslint-plugin-jsx-a11y"),
+      import: require("eslint-plugin-import"),
+      prettier: prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": "error",
+      "react/jsx-filename-extension": [1, { extensions: [".jsx", ".tsx"] }],
+      "import/no-unresolved": "off",
+      "import/extensions": "off",
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {},
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  airbnb,
+  prettier.configs.recommended,
 ];
