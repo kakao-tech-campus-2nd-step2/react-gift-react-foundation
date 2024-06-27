61a51aec0bb6d2f400e89757a4c1c072e541a003
rudtj <kyoungseo0327@gmail.com>
2024-06-27 14:56:45 +0900

    feat: reset css 적용

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Reset } from 'styled-reset';
+
+function App () {
+  return (
+    <React.Fragment>
+      <Reset />
+              <h1>Hello World</h1>
+  </React.Fragment>
+  );
+}
+
+export default App;
+
