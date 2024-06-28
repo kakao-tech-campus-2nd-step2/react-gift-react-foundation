e2230be967efe8d2d014b7cbf40d4e8cb0214387
Solbin Jung <jsolbin0724@gmail.com>
2024-06-28 17:16:09 +0900

    feat: Input 컴포넌트 구현-disabled Props 추가

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+interface InputProps {
+  disabled?: boolean;
+  invalid?: boolean;
+  size?: 'small' | 'medium' | 'large' | 'responsive';
+}
+
+const Input: React.FC<InputProps> = ({
+  disabled = false,
+  size = 'medium',
+  ...props
+}: InputProps) => {
+  const sizeClass = size === 'responsive' ? 'storybook-input--responsive' : `storybook-input--${size}`;
+  
+  return (
+    <input
+      className={`storybook-input ${sizeClass}`}
+      disabled={disabled}
+      {...props}
+    />
+  );
+};
+
+export default Input;
