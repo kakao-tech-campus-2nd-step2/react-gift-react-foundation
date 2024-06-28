28150874fe91b0d691e8b0c11b95435c765a3a89
tteokbokki-master <0jin.git@gmail.com>
2024-06-28 22:17:23 +0900

    Test: Container 컴포넌트 스토리북 추가

---
+++ src/stories/COMMON/Layout/Container/Container.stories.ts
@@ -0,0 +1,31 @@
+import Container from './Container';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'COMMON/Layout/Default',
+  component: Container,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Container>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    maxWidth: 'auto',
+    containerChildren: 'Hello, World!',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+};
+export const FullScreen: Story = {
+  args: {
+    maxWidth: '100%',
+    containerChildren: 'Hello, world!',
+    flexDirection: undefined,
+    justifyContent: undefined,
+    alignItems: undefined,
+  },
+};
