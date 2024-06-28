5bd63e36af3a38321d4c22842a01c37185a07a8f
tteokbokki-master <0jin.git@gmail.com>
2024-06-28 15:22:52 +0900

    Chore: Storybook 설치

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,7 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: [
-    'airbnb',
-    'airbnb-typescript',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
