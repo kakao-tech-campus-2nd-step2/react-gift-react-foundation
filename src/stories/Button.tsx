78c249fe3a6039ed5870a799f15367d10dbf18e5
KimKyuHoi <k546kh@gmail.com>
2024-06-28 20:20:25 +0900

    Feat: 버튼 스토리북 구현

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,48 +1,90 @@
 import React from 'react';
-import './button.css';
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
+import styled from '@emotion/styled';
+
+export interface IButtonProps {
+  size?: 'large' | 'small' | 'responsive';
+
+  theme?: 'kakao' | 'outline' | 'black' | 'lightGray' | 'darkGray';
+
+  children: React.ReactNode;
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
+const ButtonStyles = styled.button<IButtonProps>`
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+
+  ${({ theme }) =>
+    theme === 'kakao' &&
+    `
+    background-color: #ffeb00;
+    color: #333;
+  `}
+  ${({ theme }) =>
+    theme === 'black' &&
+    `
+    background-color: #333;
+    color: #fff;
+  `}
+  ${({ theme }) =>
+    theme === 'outline' &&
+    `
+      border: 1px solid #333;
+      background-color: transparent;
+      color: #1ea7fd;
+  `}
+  ${({ theme }) =>
+    theme === 'lightGray' &&
+    `
+      background-color: lightgrey;
+      color: #333;
+  `}
+  ${({ theme }) =>
+    theme === 'darkGray' &&
+    `
+      background-color: #a9a9a9;
+      color: #fff;
+  `}
+
+  ${({ size }) =>
+    size === 'small' &&
+    `
+      font-size: 12px;
+      padding: 10px 16px;
+  `}
+
+  ${({ size }) =>
+    size === 'large' &&
+    `
+      font-size: 16px;
+      padding: 12px 24px;
+  `}
+
+  @media (max-width: 768px) {
+    ${({ size }) =>
+      size === 'responsive' &&
+      `
+      font-size: 14px;
+      padding: 8px 16px;
+    `}
+  }
+`;
+
 export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
+  size = 'small',
+  theme = 'kakao',
+  children,
   ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+}: IButtonProps) => {
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <ButtonStyles type='button' theme={theme} size={size} {...props}>
+      {children}
+    </ButtonStyles>
   );
 };
