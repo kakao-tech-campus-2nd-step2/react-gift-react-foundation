3e1792213beb8b541e8a53592b465a4c3d4378e3
minji2219 <200146@jnu.ac.kr>
2024-06-28 15:08:09 +0900

    Feat: Input 컴포넌트 placeholder추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,17 +7,26 @@ interface InputProps {
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   disabled?: boolean;
+  placeholder?: string;
   width?: number;
   height?: number;
 }
 const Input = (props: InputProps) => {
-  const { value, onChange, disabled = false, width = 100, height = 30 } = props;
+  const {
+    value,
+    onChange,
+    disabled = false,
+    placeholder = '입력해주세요.',
+    width = 100,
+    height = 30,
+  } = props;
   return (
     <input
       type="text"
       value={value}
       onChange={onChange}
       disabled={disabled}
+      placeholder={placeholder}
       css={css`
         width: ${width}px;
         height: ${height}px;
