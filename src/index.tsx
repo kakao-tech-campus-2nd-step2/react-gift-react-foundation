aaa5d96916e46d42503950878592c7f3ec08fe49
gimdogyun <jasper12394@gmail.com>
2024-06-28 14:36:28 +0900

    design: reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ResetCss from "@components/common/ResetCss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
+    <ResetCss />
     <App />
   </React.StrictMode>,
 );
