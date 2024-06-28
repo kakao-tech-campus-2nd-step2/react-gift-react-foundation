640ddf70aab0fab614d878b09a3846ac6df42c67
joshuadesu <meron_pan@kakao.com>
2024-06-28 21:40:00 +0900

    Feat: Add 컴포넌트 예시

--- src/stories/Image.stories.tsx
+++ src/stories/Image.stories.tsx
@@ -28,10 +28,25 @@ const Template = (args) => <Image {...args} />
 
 // Storybook에서 확인하고 싶은 컴포넌트는 export const로 작성할 것!!!
 export const RatioSquare = Template.bind({});
-
 RatioSquare.args = {
   src: 'https://images.khan.co.kr/article/2023/05/09/news-p.v1.20230509.81d6231f8f364496a03089815eef0340.jpg',
   width: '300px',
-  ratio: '',
+  ratio: 'square',
   radius: '',
+}
+
+export const RadiusCircle = Template.bind({});
+RadiusCircle.args = {
+  src: 'https://images.khan.co.kr/article/2023/05/09/news-p.v1.20230509.81d6231f8f364496a03089815eef0340.jpg',
+  width: '300px',
+  ratio: 'square',
+  radius: 'circle',
+}
+
+export const RadiusRound = Template.bind({});
+RadiusRound.args = {
+  src: 'https://images.khan.co.kr/article/2023/05/09/news-p.v1.20230509.81d6231f8f364496a03089815eef0340.jpg',
+  width: '300px',
+  ratio: 'auto',
+  radius: '10px',
 }
