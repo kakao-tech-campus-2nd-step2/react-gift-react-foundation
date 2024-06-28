da4a595a972fec524aed1c4a8cbbbdd6d5eea321
dmsrud1218 <dmsrud566@naver.com>
2024-06-28 11:31:45 +0900

    refactor(index.tsx) : typescript변환 수정

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/reset.css';
+
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
@@ -17,4 +19,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
