2383da1199e38245ef94670dd35f855a9d58801f
diwoni <jgw117@naver.com>
2024-06-28 21:44:24 +0900

    feat: GoodsItems 컴포넌트 구현 및 Grid & Container 컴포넌트 구현(하드코딩)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import 'reset-css';
+import GoodsItem from './components/GoodsItem';
+import Grid from './components/Grid';
+import Container from './components/Container';
 
 export default function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Container>
+      <h1>Goods List</h1>
+      <Grid>
+        <GoodsItem
+          imageSrc="https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png"
+          subtitle="카카오 프렌즈 특별 한정판"
+          title="[특가] 카카오 프렌즈 특별 한정판 브라이트 쿠션"
+          amount={100000000000}
+          rankingIndex={1}
+        />
+        <GoodsItem
+          imageSrc="https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png"
+          subtitle="카카오 프렌즈 특별 한정판"
+          title="[특가] 카카오 프렌즈 특별 한정판 브라이트 쿠션"
+          amount={100000000000}
+          rankingIndex={2}
+        />
+      </Grid>
+    </Container>
+  );
 }
