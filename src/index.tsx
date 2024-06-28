c5da8cd50b8eed4420f719343bbd3c105ff762de
seorinnn <105342023+seorinnn@users.noreply.github.com>
2024-06-28 15:40:39 +0900

    feat : emotion 스타일 라이브러리 추가 및 reset css 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Global, css } from "@emotion/react";
+import reset from "./styles/reset";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
+      <Global styles={reset} />
       <App />
     </React.StrictMode>
   );
