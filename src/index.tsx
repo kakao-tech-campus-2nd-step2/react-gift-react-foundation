36e4516807d69bf142ccd6d69ea4c7872b953d86
diwoni <jgw117@naver.com>
2024-06-28 21:49:06 +0900

    refactor: index.tsx -> App.tsx 로 컴포넌트 이동 및 Grid.css 가독성을 위해 임시로 border 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App';
-import Button from './components/Button';
-import Input from './components/Input';
-import Image from './components/Image';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-    <div style={{ width: '120px' }}>
-      <Button
-        width="responsive"
-        height="100px"
-        buttonColor="#e8e92e"
-        hasBorder
-        fontColor="black"
-        fontSize="20px"
-        borderColor="#e8e92e"
-        borderRadius="5px"
-        onClick={() => alert('Click')}
-      >
-        button
-      </Button>
-      <Input
-        placeholder="입력"
-        styleProps={{ width: '200px', height: '100px' }}
-        disabled
-      />
-    </div>
-    <div style={{ width: '300px' }}>
-      <Image
-        src="https://upload.wikimedia.org/wikipedia/commons/f/f2/Kakao_logo.jpg"
-        alt="예시 이미지"
-        ratio={16 / 9}
-        radius={10}
-      />
-    </div>
   </React.StrictMode>,
 );
