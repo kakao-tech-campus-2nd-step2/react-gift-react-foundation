e7af12a028ab6599b5c38335da71448f82309626
Jisu Lee <dlwltn0430@naver.com>
2024-06-28 19:24:51 +0900

    feat: Input 컴포넌트 구현

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react'
 import Button from './Button'
 
 const meta: Meta<typeof Button> = {
-   title: 'COMMON/Button',
+   title: 'Common/Button',
    component: Button,
    tags: ['autodocs'],
 }
