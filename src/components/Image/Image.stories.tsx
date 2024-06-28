c1848f90e9ac04d039f2703885a819850a83a440
faddishcorn <juni129461@gmail.com>
2024-06-28 23:27:38 +0900

    build: Image스토리 재작성

--- src/components/Image/Image.stories.tsx
+++ src/components/Image/Image.stories.tsx
@@ -1,56 +1,71 @@
-/** @jsxImportSource @emotion/react */
 import React from 'react';
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import { StoryObj, Meta } from '@storybook/react';
+import Image, { ImageProps } from './Image';
 
-type Ratio = 'square' | 'auto' | number;
-type Radius = 'circle' | number;
+export default {
+  title: 'Common/Image',
+  component: Image,
+  argTypes: {
+    ratio: {
+      control: 'select',
+      options: ['square', 'auto', 16 / 9, 4 / 3], // 사용자가 값을 지정할 수 있습니다.
+    },
+    radius: {
+      control: 'select',
+      options: ['circle', 0, 10, 20, 30],
+    },
+    src: {
+      control: 'text',
+    },
+    alt: {
+      control: 'text',
+    },
+  },
+} as Meta<typeof Image>;
 
-export type ImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
-  ratio?: Ratio;
-  radius?: Radius;
+type Story = StoryObj<ImageProps>;
+
+export const Square: Story = {
+  args: {
+    ratio: 'square',
+    radius: 0,
+    src: 'https://via.placeholder.com/300',
+    alt: 'Square Image',
+  },
 };
 
-const ratioStyles = (ratio: Ratio) => {
-  if (ratio === 'square') {
-    return css`
-      aspect-ratio: 1 / 1;
-    `;
-  }
-  if (ratio === 'auto') {
-    return css`
-      width: auto;
-      height: auto;
-    `;
-  }
-  return css`
-    aspect-ratio: ${ratio};
-  `;
+export const Circle: Story = {
+  args: {
+    ratio: 'square',
+    radius: 'circle',
+    src: 'https://via.placeholder.com/300',
+    alt: 'Circle Image',
+  },
 };
 
-const radiusStyles = (radius: Radius) => {
-  if (radius === 'circle') {
-    return css`
-      border-radius: 50%;
-    `;
-  }
-  return css`
-    border-radius: ${radius}px;
-  `;
+export const SixteenByNine: Story = {
+  args: {
+    ratio: 16 / 9,
+    radius: 0,
+    src: 'https://via.placeholder.com/300',
+    alt: '16:9 Image',
+  },
 };
 
-const StyledImage = styled.img<ImageProps>`
-  width: 100%;
-  ${({ ratio }) => ratio && ratioStyles(ratio)}
-  ${({ radius }) => radius && radiusStyles(radius)}
-`;
-
-const Image: React.FC<ImageProps> = ({
-  ratio = 'auto',
-  radius = 0,
-  ...props
-}) => {
-  return <StyledImage ratio={ratio} radius={radius} {...props} />;
+export const RoundedCorners: Story = {
+  args: {
+    ratio: 4 / 3,
+    radius: 20,
+    src: 'https://via.placeholder.com/300',
+    alt: 'Rounded Corners Image',
+  },
 };
 
-export default Image;
+export const Auto: Story = {
+  args: {
+    ratio: 'auto',
+    radius: 0,
+    src: 'https://via.placeholder.com/300',
+    alt: 'Auto Image',
+  },
+};
