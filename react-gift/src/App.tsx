af8348a002257620f9be0cec0f8a69f981e29f43
eunjin <eunjin0321@naver.com>
2024-06-28 18:44:26 +0900

    build: Input 컴포넌트 추가

--- react-gift/src/App.tsx
+++ react-gift/src/App.tsx
@@ -1,6 +1,17 @@
+import React, { useState, ChangeEvent } from 'react';
 import Button from './components/Button';
+import Input from './components/Input';
 
 function App() {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isInvalid, setIsInvalid] = useState<boolean>(false);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setInputValue(value);
+    setIsInvalid(value.length > 0 && value.length < 5);
+  };
+
   const handleClick = () => {
     alert('Button clicked!');
   };
@@ -10,6 +21,14 @@ function App() {
       <Button state="normal" size="small" colorType="black" onClick={handleClick}>
         버튼
       </Button><br/>
+      <Input 
+        value={inputValue} 
+        onChange={handleInputChange} 
+        placeholder="작성하세용" 
+        inputsize="medium"
+        invalid={isInvalid}
+        disabled={false}
+      />
     </div>
   );
 }
---
+++ react-gift/src/components/Input.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Size, disabledStyle, invalidStyle} from '../styles/InputStyle';
+
+type InputStyleProps = {
+  inputsize: 'small' | 'medium' | 'large' | 'responsive';
+  disabled?: boolean;
+  invalid?: boolean;
+};
+
+const InputStyle = styled.input<InputStyleProps>`
+  border-radius: 5px;
+  ${(props) => Size[props.inputsize]}
+  ${(props) => props.disabled && disabledStyle}
+  ${(props) => props.invalid && invalidStyle}
+`;
+
+interface InputProps extends InputStyleProps, React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = ({ inputsize, disabled, invalid, ...props }:InputProps) => (
+  <InputStyle inputsize={inputsize} disabled={disabled} invalid={invalid} {...props} />
+);
+
+Input.defaultProps = {
+  inputsize: 'medium',
+  disabled: false,
+  invalid: false,
+};
+
+export default Input;
