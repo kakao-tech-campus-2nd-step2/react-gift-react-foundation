18bab6020f211ef68f52d0860c188f6df2b57e55
Kuheewon <selly3109@naver.com>
2024-06-28 15:03:14 +0900

    Revert "refactoring. 모듈 추가"
    This reverts commit 966efc34edfe9a24c2462b9de2a4a0351956f185.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,15 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: [
-    '../src/**/*.mdx',
-    '../src/Button/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/GoodsItem/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/Grid/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/Image/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/Input/**/*.stories.@(js|jsx|ts|tsx)',
-    '../src/Layout/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-onboarding',
