414e7cbd51d03ef5fdbd5f5cfbd26f1ce6ba5c5b
anheejeong <eyrt6973@naver.com>
2024-06-28 17:42:09 +0900

    [Feat] Ranking 컴포넌트 구현

--- src/components/GoodsItem/Ranking/Ranking.tsx
+++ src/components/GoodsItem/Ranking/Ranking.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ranking.css';
 
 interface GoodsItemProps {
+    rankingIndex: number;
     imageSrc: string; // 이미지 링크
     subtitle?: string,
     title?: string,
@@ -9,6 +10,7 @@ interface GoodsItemProps {
 }
 
 export const Ranking = ({
+    rankingIndex,
     imageSrc = 'https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png',
     subtitle,
     title,
@@ -17,9 +19,12 @@ export const Ranking = ({
 }: GoodsItemProps) => {
 
     return (
-        <img
-            src={imageSrc}
-            {...props}
-        />
+        <div className='storybook-ranking'>
+            <span className='storybook-ranking--ranking'>{rankingIndex}</span>
+            <img src={imageSrc} />
+            <span className='storybook-ranking--sub'>{subtitle}</span>
+            <span className='storybook-ranking--title'>{title}</span>
+            <span className='storybook-ranking--amount'>{amount}원</span>
+        </div>
     );
 };
