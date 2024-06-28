aa590493b70e96ea0b2c5f9661c5134a1fcdbea3
JaeAnHan <jaean1999@naver.com>
2024-06-28 12:38:43 +0900

    feat : add .css on webpack resolve extensions

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,82 @@
-const path = require("path");
+const path = require('path');
 // 없으면 CORS 뜸
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "./src/index.tsx"),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx|ts|tsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            "@babel/preset-env",
-                            ["@babel/preset-react", {"runtime": "automatic"}],
-                            "@babel/preset-typescript"
-                        ],
-                        plugins: [
-                            "@babel/plugin-transform-react-jsx"
-                        ]
-                    }
-                }
+  mode: 'development',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+              '@babel/preset-typescript',
+            ],
+            plugins: ['@babel/plugin-transform-react-jsx'],
+          },
+        },
+      },
+      // style-loader, css-loader 구성
+      {
+        test: /\.css$/i,
+        exclude: /\.module\.css$/i, // 모듈 파일 제외 설정
+        use: ['style-loader', 'css-loader'],
+      },
+      // CSS Module ([filename].module.css)
+      {
+        test: /\.module\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
             },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader' },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|ico)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[hash][ext][query]',
-                },
-            }
+          },
         ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|ico)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.jsx', '.tsx', '.css'],
+    alias: {
+      components: path.resolve(__dirname, 'src/components/'),
+      assets: path.resolve(__dirname, 'src/assets/'),
+      utils: path.resolve(__dirname, 'src/utils/'),
+      pages: path.resolve(__dirname, 'src/pages/'),
+      hooks: path.resolve(__dirname, 'src/hooks/'),
+      store: path.resolve(__dirname, 'src/store'),
     },
-    resolve: {
-        extensions: [ ".ts", ".js", ".jsx", ".tsx"],
-    },
-    output: {
-        publicPath: '/',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-        clean: true,
-    },
-    devServer: {
-        historyApiFallback: true,
-        port: 3000,
-        hot: true,
-        static: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-            filename: 'index.html',
-        }),
-    ]
+  },
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+    static: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
