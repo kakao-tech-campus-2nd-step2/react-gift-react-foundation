e67b651b4759b2ea652cac9dfef9c7d95fa515e3
faddishcorn <juni129461@gmail.com>
2024-06-28 23:29:56 +0900

    build: 컨테이너 컴포넌트 재작성

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -1,42 +1,51 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
-import { StoryObj, Meta } from '@storybook/react';
-import Grid from './Grid';
+import styled from '@emotion/styled';
 
-export default {
-  title: 'Layout/Grid',
-  component: Grid,
-  argTypes: {
-    gap: { control: 'number' },
-    columns: {
-      control: 'object',
-    },
-    children: { control: 'object' },
-  },
-} as Meta<typeof Grid>;
-
-type Story = StoryObj<React.ComponentProps<typeof Grid>>;
-
-const Template: Story = (args) => (
-  <Grid {...args}>
-    {[...Array(16).keys()].map((num) => (
-      <div key={num + 1}>{num + 1}</div>
-    ))}
-  </Grid>
-);
-
-export const Default: Story = Template.bind({});
-Default.args = {
-  gap: 16,
-  columns: 4,
+type ContainerProps = {
+  padding?: string;
+  maxWidth?: string;
+  flexDirection?: 'row' | 'column';
+  justifyContent?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around';
+  alignItems?: 'center' | 'flex-start' | 'flex-end' | 'baseline' | 'stretch';
+  children?: React.ReactNode;
 };
 
-export const Responsive: Story = Template.bind({});
-Responsive.args = {
-  gap: 16,
-  columns: {
-    initial: 1,
-    sm: 2,
-    md: 3,
-    lg: 4,
-  },
+const StyledContainer = styled.div<ContainerProps>`
+  display: flex;
+  flex-direction: ${({ flexDirection }) => flexDirection || 'row'};
+  justify-content: ${({ justifyContent }) => justifyContent || 'flex-start'};
+  align-items: ${({ alignItems }) => alignItems || 'stretch'};
+  width: 100%;
+  max-width: ${({ maxWidth }) => maxWidth || '1200px'};
+  margin: 0 auto;
+  padding: ${({ padding }) => padding || '16px'};
+`;
+
+const Container: React.FC<ContainerProps> = ({
+  padding = '16px',
+  maxWidth = '1200px',
+  flexDirection = 'row',
+  justifyContent = 'flex-start',
+  alignItems = 'stretch',
+  children,
+}) => {
+  return (
+    <StyledContainer
+      padding={padding}
+      maxWidth={maxWidth}
+      flexDirection={flexDirection}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
+    >
+      {children}
+    </StyledContainer>
+  );
 };
+
+export default Container;
