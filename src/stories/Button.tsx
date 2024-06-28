6f5a8d8685c9f2ee0039bc9bfed82ff694b51c1f
Jisu Lee <dlwltn0430@naver.com>
2024-06-28 17:51:53 +0900

    feat: Theme Props에 따라 버튼의 컬러와 디자인 달라지도록 구현

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,48 +1,54 @@
-import React from 'react';
-import './button.css';
+import React from 'react'
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+   theme?: 'kakao' | 'outline' | 'black' | 'lightGray' | 'darkGray'
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+const themeTypes = {
+   kakao: css`
+      background-color: rgb(254, 229, 0);
+      color: rgb(17, 17, 17);
+   `,
+   outline: css`
+      background-color: transparent;
+      color: rgb(17, 17, 17);
+      box-shadow: rgb(204, 204, 204) 0px 0px 0px 1px inset;
+   `,
+   black: css`
+      background-color: rgb(17, 17, 17);
+      color: #fff;
+   `,
+   lightGray: css`
+      background-color: rgb(240, 240, 240);
+      color: rgb(17, 17, 17);
+   `,
+   darkGray: css`
+      background-color: rgb(68, 68, 68);
+      color: #fff;
+   `,
+}
+
+const CustomButton = styled.button<ButtonProps>`
+   ${(props) => props.theme && themeTypes[props.theme as keyof typeof themeTypes]}
+
+   border: none;
+   border-radius: 4px;
+   width: 100px;
+   height: 40px;
+   cursor: pointer;
+   &:hover {
+      opacity: 0.8;
+   }
+`
+
+const Button: React.FC<ButtonProps> = ({ children, theme = 'kakao', ...props }) => {
+   return (
+      <CustomButton theme={theme} {...props}>
+         {children || '버튼'}
+      </CustomButton>
+   )
+}
+
+export default Button
