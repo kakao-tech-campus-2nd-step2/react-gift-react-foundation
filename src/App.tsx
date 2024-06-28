0df4d9d2b464af145bfa3133fb70a76d75e18d51
박건규 <geongyu09@gmail.com>
2024-06-28 23:34:19 +0900

    refactor: 사용하지 않는 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
 import React from "react";
-import Button from "components/common/Button/Button";
-import Input from "components/common/Input";
-import Image from "components/common/Image/Image";
 
 export default function App() {
-  return (
-    <div>
-      <Button theme="primary" text="Button" />
-      <Input size="small" />
-      <Image ratio={12 / 2} radius={0} />
-    </div>
-  );
+  return <div></div>;
 }
