e49cbbe739e07af567e91702d65675bc81de9aa0
JaeAnHan <jaean1999@naver.com>
2024-06-28 15:02:04 +0900

    feat : add Input Component (step 2 rqmt)
    - add UnderlineTextField.tsx
    - add UnderlineTextField.stories.ts

---
+++ src/components/Input/UnderlineTextField.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'components/Input/underlinetextField.css';
+
+interface InputProps {
+  disabled: boolean;
+  invalid: boolean;
+  size: 'small' | 'medium' | 'large' | 'responsive';
+  placeholder?: string;
+}
+
+export const UnderlineTextField = ({
+  disabled,
+  size,
+  invalid,
+  placeholder = '',
+}: InputProps) => {
+  return (
+    <input
+      disabled={disabled}
+      placeholder={placeholder}
+      className={['underline--input', size, invalid ? 'invalid' : ''].join(' ')}
+      autoComplete={'off'}
+    />
+  );
+};
---
+++ src/stories/UnderlineTextField.stories.ts
@@ -0,0 +1,33 @@
+import { UnderlineTextField } from 'components/Input/UnderlineTextField';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Example/Input',
+  component: UnderlineTextField,
+  tags: ['autodocs'],
+  argTypes: {
+    disabled: {
+      control: 'boolean',
+    },
+    invalid: {
+      control: 'boolean',
+    },
+    size: {
+      control: 'select',
+      options: ['responsive', 'small', 'medium', 'large'],
+    },
+  },
+  args: {},
+} satisfies Meta<typeof UnderlineTextField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    disabled: false,
+    invalid: false,
+    size: 'medium',
+    placeholder: 'placeholder',
+  },
+};
