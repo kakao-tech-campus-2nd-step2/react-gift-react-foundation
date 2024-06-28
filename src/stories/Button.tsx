a079c28d6e8a1f3f90d4519b42b3598cabfbb5f7
JaeAnHan <jaean1999@naver.com>
2024-06-28 10:10:17 +0900

    feat : implement Button regarding to step2 requirements
    - theme : kakao, light, dark
    - size : small, medium, large, responsive

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -6,40 +6,30 @@ interface ButtonProps {
    * Is this the principal call to action on the page?
    */
   primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
+  theme?: string;
+  size?: 'small' | 'medium' | 'large' | 'responsive';
   label: string;
-  /**
-   * Optional click handler
-   */
   onClick?: () => void;
 }
 
-/**
- * Primary UI component for user interaction
- */
 export const Button = ({
   primary = false,
   size = 'medium',
-  backgroundColor,
+  theme,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const firstMeetBorder = primary ? 'first' : '';
+
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      className={[
+        'storybook-button',
+        `storybook-button--${size}`,
+        theme,
+        firstMeetBorder,
+      ].join(' ')}
       {...props}
     >
       {label}
