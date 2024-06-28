849b32f3d83ac6ddabb0b0f332ad6ce87ae0ac37
JaeAnHan <jaean1999@naver.com>
2024-06-28 12:52:16 +0900

    fix : fix path alias on story webpack

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import * as path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -13,6 +14,19 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
+  },
+  webpackFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      components: path.resolve(__dirname, '../src/components/'),
+      assets: path.resolve(__dirname, '../src/assets/'),
+      utils: path.resolve(__dirname, '../src/utils/'),
+      pages: path.resolve(__dirname, '../src/pages/'),
+      hooks: path.resolve(__dirname, '../src/hooks/'),
+      store: path.resolve(__dirname, '../src/store'),
+    };
+
+    return config;
   },
 };
 export default config;
