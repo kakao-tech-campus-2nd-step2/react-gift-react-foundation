99e446a06dbdd0b10eb7c0f37213dda8eb199d54
tteokbokki-master <0jin.git@gmail.com>
2024-06-28 19:40:01 +0900

    Test: GoodsItem/Default/Item 컴포넌트 스토리북 추가

---
+++ src/stories/GoodsItem/Default/Item.stories.ts
@@ -0,0 +1,22 @@
+import Item from './Item';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'COMMON/GoodSItems/Default',
+  component: Item,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Item>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    imageSrc:
+      'https://t1.kakaocdn.net/friends/www/talk/kakaofriends_talk_2018.png',
+    subtitle: '카카오 프렌즈 특별 한정판',
+    title: '[특가] 카카오 프렌즈 특별 한정판 브라이트 쿠션',
+    amount: 100000000000,
+  },
+};
