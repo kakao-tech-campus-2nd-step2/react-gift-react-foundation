fc3fcf10396b050c1f61cbaf40b4e050ca25bca5
Solbin Jung <jsolbin0724@gmail.com>
2024-06-28 17:24:18 +0900

    feat: Input 컴포넌트 구현-invalid Props 추가

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -3,19 +3,21 @@ import React from 'react';
 interface InputProps {
   disabled?: boolean; // disabled prop 추가
   invalid?: boolean; // invalid prop 추가
-  size?: 'small' | 'medium' | 'large' | 'responsive';
+  size?: 'small' | 'medium' | 'large' | 'responsive'; // size prop 추가
 }
 
 const Input: React.FC<InputProps> = ({
   disabled = false, // 기본값으로 false 설정
+  invalid = false,
   size = 'medium',
   ...props
 }: InputProps) => {
   const sizeClass = size === 'responsive' ? 'storybook-input--responsive' : `storybook-input--${size}`;
+  const invalidClass = invalid ? 'storybook-input--invalid' : ''; // invalid prop을 기반으로 클래스 설정
   
   return (
     <input
-      className={`storybook-input ${sizeClass}`}
+      className={`storybook-input ${sizeClass} ${invalidClass}`}
       disabled={disabled} //input 요소에 적용
       {...props}
     />
