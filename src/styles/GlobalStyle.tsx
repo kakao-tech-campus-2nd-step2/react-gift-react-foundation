0017031357f6e0e0ba3b9d19848291df84ede024
KimKyuHoi <k546kh@gmail.com>
2024-06-28 12:46:06 +0900

    Design: Global Style 적용

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -6,11 +6,16 @@ const Reset = css`
     box-sizing: border-box;
     font-family: 'Noto Sans KR', sans-serif;
     font-size: 16px;
+    line-height: 1.5;
     margin: 0;
     padding: 0;
     outline: none;
     list-style: none;
     text-decoration: none;
+  }
+
+  img {
+    display: block;
   }
 `;
 const GlobalStyle = () => {
