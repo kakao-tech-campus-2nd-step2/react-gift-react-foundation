2b8b26a7122dc49585c4cfb1294e2d08631f80a0
JeongSeoyun <xeonj@naver.com>
2024-06-29 20:34:33 +0900

    chore: storybook 절대 경로 지정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -14,6 +15,18 @@ const config: StorybookConfig = {
     name: '@storybook/react-webpack5',
     options: {}
   },
-  staticDirs: ['../public']
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [
+        ...(config.resolve.plugins || []),
+        new TsconfigPathsPlugin({
+          extensions: config.resolve.extensions,
+        }),
+      ];
+    }
+    
+    return config;
+  }
 };
 export default config;
