ebd199c9c4deee8ccebc36a8568797fe73ab663b
JeongSeoyun <xeonj@naver.com>
2024-06-29 20:28:52 +0900

    feat: Container.tsx 변수 선언 없이 prop 전달 방식으로 변경

--- src/Container.tsx
+++ src/Container.tsx
@@ -11,14 +11,11 @@ const Container: React.FC<ContainerProps> = ({
   children,
   maxWidth = '1200px'
 }) => {
-  const containerStyle: React.CSSProperties = {
-    maxWidth,
-    margin: '0 auto',
-    padding: '0 20px'
-  };
-
   return (
-    <div className="container" style={containerStyle}>
+    <div
+      className="container"
+      style={{ maxWidth, margin: '0 auto', padding: '0 20px' }}
+    >
       {children}
     </div>
   );
