fbbc22fc06f977d07c1a4895db66b6a5272a2d39
Solbin Jung <jsolbin0724@gmail.com>
2024-06-30 16:34:21 +0900

    feat: Grid 컴포넌트 폴더 구조 및 내용 수정

--- my-app/src/stories/Grid.stories.tsx
+++ my-app/src/stories/Grid.stories.tsx
@@ -3,24 +3,45 @@ import { Meta, StoryFn } from '@storybook/react';
 import Grid from './Grid';
 
 const meta: Meta = {
-  title: 'Common/Grid',
+  title: 'Common/Layuot/Grid',
   component: Grid,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
 };
 
 export default meta;
 
 const Template: StoryFn = (args) => <Grid columns={0} children={undefined} {...args} />;
 
-export const Default = Template.bind({});
-Default.args = {
+export const NumberColumns = Template.bind({});
+NumberColumns.args = {
   columns: 3,
-  gap: 16,
+  gap: 0,
   children: Array.from({ length: 16 }, (_, index) => (
     <div
       key={index}
       style={{
         padding: '10px',
-        textAlign: 'center',
+        textAlign: 'left',
+      }}
+    >
+      {index + 1}
+    </div>
+  )),
+};
+
+export const ResponsiveColumns = Template.bind({});
+ResponsiveColumns.args = {
+  columns: 0,
+  gap: 0,
+  children: Array.from({ length: 16 }, (_, index) => (
+    <div
+      key={index}
+      style={{
+        padding: '10px',
+        textAlign: 'left',
       }}
     >
       {index + 1}
