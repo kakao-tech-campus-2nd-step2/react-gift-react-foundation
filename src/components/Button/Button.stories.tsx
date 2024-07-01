3d810d4925ac4b710e284520f2863a44bbda7e13
rudtj <kyoungseo0327@gmail.com>
2024-07-01 15:24:47 +0900

    feat: ButtonStory 수정

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { Meta, StoryFn } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import Button, { ButtonProps } from './Button.tsx';
 
 export default {
@@ -8,7 +8,10 @@ export default {
   component: Button,
   argTypes: {
     size: {
-      control: { type: 'select', options: ['small', 'medium', 'large', 'responsive'] },
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large', 'responsive'],
+      },
     },
     theme: {
       control: { type: 'select', options: ['primary', 'secondary', 'kakao'] },
@@ -17,37 +20,42 @@ export default {
   },
 } as Meta;
 
-// eslint-disable-next-line react/function-component-definition
-const Template: StoryFn<ButtonProps> = (args: ButtonProps) => (
-  <div style={{ width: '120px' }}>
-    <Button {...args}>버튼</Button>
-  </div>
-);
+function Template(args: ButtonProps) {
+  return (
+    <div style={{ width: '120px' }}>
+      <Button {...args}>버튼</Button>
+    </div>
+  );
+}
 
-export const Primary = Template.bind({});
-Primary.args = {
-  size: 'medium',
-  theme: 'primary',
-  onClick: () => action('Primary button clicked'),
+export const Primary: StoryObj<typeof Template> = {
+  args: {
+    size: 'medium',
+    theme: 'primary',
+    onClick: () => action('Primary button clicked'),
+  },
 };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  size: 'medium',
-  theme: 'secondary',
-  onClick: () => action('Secondary button clicked'),
+export const Secondary: StoryObj<typeof Template> = {
+  args: {
+    size: 'medium',
+    theme: 'secondary',
+    onClick: () => action('Secondary button clicked'),
+  },
 };
 
-export const Kakao = Template.bind({});
-Kakao.args = {
-  size: 'medium',
-  theme: 'kakao',
-  onClick: () => action('Kakao button clicked'),
+export const Kakao: StoryObj<typeof Template> = {
+  args: {
+    size: 'medium',
+    theme: 'kakao',
+    onClick: () => action('Kakao button clicked'),
+  },
 };
 
-export const Responsive = Template.bind({});
-Responsive.args = {
-  size: 'responsive',
-  theme: 'primary',
-  onClick: () => action('Responsive button clicked'),
+export const Responsive: StoryObj<typeof Template> = {
+  args: {
+    size: 'responsive',
+    theme: 'primary',
+    onClick: () => action('Responsive button clicked'),
+  },
 };
