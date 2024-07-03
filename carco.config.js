a9cc653d5ef71c9d1dfda6dbc0b71d71dc98fd7e
joojjang <itchu999@naver.com>
2024-07-03 17:48:21 +0900

    fix(alias): reconfigure absolute paths

--- carco.config.js
+++ carco.config.js
@@ -5,6 +5,7 @@ module.exports = {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
 };
