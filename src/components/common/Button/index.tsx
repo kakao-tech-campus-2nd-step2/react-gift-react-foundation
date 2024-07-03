228929836bb141cfc0758a894d10c9ae6e6cab0f
dmsrud1218 <dmsrud566@naver.com>
2024-07-04 04:10:00 +0900

    Chore : 주석설명 달기추가

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,16 +1,18 @@
 import styled from '@emotion/styled';
 
-import { breakpoints } from '@/styles/variants';
+import { breakpoints } from '@/styles/variants'; // breakpoints는 미디어 쿼리를 위해 사용됩니다.
 
 type Props = {
-  theme?: 'kakao' | 'outline' | 'black' | 'lightGray' | 'darkGray';
-  size?: 'large' | 'small' | 'responsive';
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+  theme?: 'kakao' | 'outline' | 'black' | 'lightGray' | 'darkGray'; // 버튼 테마 정의
+  size?: 'large' | 'small' | 'responsive'; // 버튼 사이즈
+} & React.ButtonHTMLAttributes<HTMLButtonElement>; // 버튼의 기본 속성 지원
 
+// Button 컴포넌트 정의, props를 통해 스타일링을 적용
 export const Button: React.FC<Props> = ({ ...props }: Props) => {
-  return <Wrapper {...props} />;
+  return <Wrapper {...props} />; // Wrapper 컴포넌트를 사용하여 스타일링된 버튼을 반환
 };
 
+// Wrapper 컴포넌트를 styled.button을 사용하여 스타일링
 const Wrapper = styled.button<Pick<Props, 'theme' | 'size'>>(
   {
     width: '100%',
@@ -21,6 +23,7 @@ const Wrapper = styled.button<Pick<Props, 'theme' | 'size'>>(
     cursor: 'pointer',
     transition: 'background-color 200ms',
   },
+  // 사이즈별 스타일 정의
   ({ size = 'responsive' }) => {
     const largeStyle = {
       height: '60px',
@@ -38,7 +41,7 @@ const Wrapper = styled.button<Pick<Props, 'theme' | 'size'>>(
     if (size === 'small') {
       return smallStyle;
     }
-
+    // size가 'responsive'인 경우 미디어 쿼리를 통해 스타일 적용
     return {
       ...smallStyle,
       [`@media screen and (min-width:${breakpoints.sm})`]: {
@@ -46,6 +49,7 @@ const Wrapper = styled.button<Pick<Props, 'theme' | 'size'>>(
       },
     };
   },
+  // 테마별 스타일 정의
   ({ theme = 'kakao' }) => {
     if (theme === 'outline') {
       return {
