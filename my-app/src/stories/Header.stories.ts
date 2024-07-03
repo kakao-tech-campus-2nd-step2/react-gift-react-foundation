695e351396f72a772883a8d155e3cbe03b45665a
yeramm <213187@jnu.ac.kr>
2024-07-03 17:25:05 +0900

    fix: storybook 실행시 발생했던 컴파일 오류 수정
    storybook 실행시 발생했던 eslint-config-prettier 관련 오류 해결했으나
    prettier 관련 오류 발생

--- my-app/src/stories/Header.stories.ts
+++ my-app/src/stories/Header.stories.ts
@@ -10,13 +10,13 @@ const meta: Meta<typeof Header> = {
   tags: ['autodocs'],
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
+    layout: 'fullscreen'
   },
   args: {
     onLogin: fn(),
     onLogout: fn(),
-    onCreateAccount: fn(),
-  },
+    onCreateAccount: fn()
+  }
 };
 
 export default meta;
@@ -25,9 +25,9 @@ type Story = StoryObj<typeof Header>;
 export const LoggedIn: Story = {
   args: {
     user: {
-      name: 'Jane Doe',
-    },
-  },
+      name: 'Jane Doe'
+    }
+  }
 };
 
 export const LoggedOut: Story = {};
--- my-app/src/stories/Page.stories.ts
+++ my-app/src/stories/Page.stories.ts
@@ -8,8 +8,8 @@ const meta: Meta<typeof Page> = {
   component: Page,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/configure/story-layout
-    layout: 'fullscreen',
-  },
+    layout: 'fullscreen'
+  }
 };
 
 export default meta;
@@ -28,5 +28,5 @@ export const LoggedIn: Story = {
 
     const logoutButton = canvas.getByRole('button', { name: /Log out/i });
     await expect(logoutButton).toBeInTheDocument();
-  },
+  }
 };
