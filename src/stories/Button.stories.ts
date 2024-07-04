949056c7c85640dda9d088d3ec52d5dcc71cdd8b
leedohyun <leedo573@gmail.com>
2024-07-04 15:59:05 +0900

    fix : ButtonProps로 상속 구현

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
 import { Button } from './Button';
+import { action } from '@storybook/addon-actions';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
-  title: 'Example/Button',
+const meta: Meta<typeof Button> = {
+  title: 'Common/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
@@ -15,10 +15,16 @@ const meta = {
   // More on argTypes: https://storybook.js.org/docs/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
+    theme: {
+      control: {
+        type: 'select',
+        options: ['kakao', 'outline', 'black', 'lightGray', 'darkGray'],
+      },
+    },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+  // Use `action` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: action('button-click') },
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -48,5 +54,47 @@ export const Small: Story = {
   args: {
     size: 'small',
     label: 'Button',
+  },
+};
+
+export const Responsive: Story = {
+  args: {
+    size: 'responsive',
+    label: 'Responsive Button',
+  },
+};
+
+export const Kakao: Story = {
+  args: {
+    theme: 'kakao',
+    label: 'Kakao Button',
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    theme: 'outline',
+    label: 'Outline Button',
+  },
+};
+
+export const Black: Story = {
+  args: {
+    theme: 'black',
+    label: 'Black Button',
+  },
+};
+
+export const LightGray: Story = {
+  args: {
+    theme: 'lightGray',
+    label: 'Light Gray Button',
+  },
+};
+
+export const DarkGray: Story = {
+  args: {
+    theme: 'darkGray',
+    label: 'Dark Gray Button',
   },
 };
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { ComponentProps } from 'react';
 import './button.css';
 
-interface ButtonProps {
+interface ButtonProps extends ComponentProps<'button'> {
   /**
    * Is this the principal call to action on the page?
    */
@@ -13,33 +13,45 @@ interface ButtonProps {
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large' | 'responsive';
   /**
    * Button contents
    */
   label: string;
   /**
-   * Optional click handler
+   * Custom theme for the button
    */
-  onClick?: () => void;
+  theme?: 'kakao' | 'outline' | 'black' | 'lightGray' | 'darkGray';
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
+export const Button: React.FC<ButtonProps> = ({
   primary = false,
   size = 'medium',
   backgroundColor,
   label,
+  theme,
+  onClick,
   ...props
-}: ButtonProps) => {
+}) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const themeClass = theme ? `storybook-button--${theme}` : '';
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    if (onClick) {
+      onClick(event);
+    }
+    alert('버튼 클릭');
+  };
+
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={['storybook-button', `storybook-button--${size}`, mode, themeClass].join(' ')}
       style={{ backgroundColor }}
+      onClick={handleClick}
       {...props}
     >
       {label}
